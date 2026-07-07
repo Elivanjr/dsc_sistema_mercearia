198c7e177322b5655eed49096d73c6f46f1f1673
Elivan Junior <elivanvljr@gmail.com>
2026-07-07 11:11:02 -0300

    refactor: adiciona métodos para fechar conexões com o banco

--- SistemaMercearia/src/main/java/br/com/sistemamercearia/util/DatabaseConnection.java
+++ SistemaMercearia/src/main/java/br/com/sistemamercearia/util/DatabaseConnection.java
@@ -6,19 +6,60 @@ package br.com.sistemamercearia.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
  * @author elivan
  */
 public class DatabaseConnection {
-  private static final String URL = "jdbc:mysql://localhost:3306/smart_mercearia";
 
+  private static final String URL = "jdbc:mysql://localhost:3306/smart_mercearia";
   private static final String USER = "root";
   private static final String PASSWORD = "";
 
   public static Connection getConnection() throws SQLException {
-    return DriverManager.getConnection(URL, USER, PASSWORD);
+    try {
+      Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+      return connection;
+    } catch (Exception e) {
+      throw new SQLException("Erro ao conectar ao banco de dados: " + e.getMessage(), e);
+    }
+  }
+
+  public static void closeConnection(Connection conn) {
+    if (conn != null) {
+      try {
+        conn.close();
+      } catch (SQLException e) {
+        throw new RuntimeException("Erro ao fechar a conexão com o banco de dados: " + e.getMessage(), e);
+      }
+    }
+  }
+
+  public static void closeConnection(Connection conn, Statement stmt) {
+    try {
+      if (stmt != null) {
+        stmt.close();
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException("Erro ao fechar o statement: " + e.getMessage(), e);
+    } finally {
+      closeConnection(conn);
+    }
+  }
+
+  public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) {
+    try {
+      if (rs != null) {
+        rs.close();
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException("Erro ao fechar o result set: " + e.getMessage(), e);
+    } finally {
+      closeConnection(conn, stmt);
+    }
   }
 }
